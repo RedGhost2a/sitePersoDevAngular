b21e6a5ab75cf7e7bbff23ca2dd8cd2a7548b693
RedGhost2a <pinpin2a@live.fr>
2023-06-20 21:58:57 +0200

    add blog et contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ContactFormComponent} from "./contact-form/contact-form.component";
 import {BlogComponent} from "./blog/blog.component";
+import {AdminComponent} from "./admin/admin.component";
 
 const routes: Routes = [
   { path: 'contact', component: ContactFormComponent },
   { path: 'blog', component: BlogComponent },
+  { path: 'admin', component: AdminComponent },
   { path: '', redirectTo: '/blog', pathMatch: 'full' },
+  // { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
 
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import { BlogComponent } from './blog/blog.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactFormComponent,
-    BlogComponent
+    BlogComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

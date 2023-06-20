f174bf5f2ca1b869220c457227e9afbef7cd1cd6
RedGhost2a <pinpin2a@live.fr>
2023-06-20 23:01:06 +0200

    add socket

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import {ContactFormComponent} from "./contact-form/contact-form.component";
 import {BlogComponent} from "./blog/blog.component";
 import {AdminComponent} from "./admin/admin.component";
+import {ChatMessageComponent} from "./chat-message/chat-message.component";
 
 const routes: Routes = [
   { path: 'contact', component: ContactFormComponent },
   { path: 'blog', component: BlogComponent },
   { path: 'admin', component: AdminComponent },
+  { path: 'chatMessage', component: ChatMessageComponent },
   { path: '', redirectTo: '/blog', pathMatch: 'full' },
   // { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import { BlogComponent } from './blog/blog.component';
 import { AdminComponent } from './admin/admin.component';
+import { ChatMessageComponent } from './chat-message/chat-message.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactFormComponent,
     BlogComponent,
-    AdminComponent
+    AdminComponent,
+    ChatMessageComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
